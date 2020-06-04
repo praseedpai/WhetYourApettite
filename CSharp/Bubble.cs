67db8707e0e2dff27edc574d70871787e45d3ff4
praseedpai <praseedp@gmail.com>
2020-06-04 10:44:22 +0530

    saved

---
+++ CSharp/Bubble.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    
+     
+    
+    static class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            if ( args.Length == 0 )
+	    {
+                  Console.WriteLine("No Command Line ARguments");
+                  return;
+
+            }
+
+            int [] arr = new int[args.Length];
+
+            for( int i=0; i< arr.Length ; ++i )
+                 arr[i] = Convert.ToInt32(args[i]);
+
+		int n = arr.Length;
+              for(int i = 0; i<n; ++i)
+                for (int j = 0; j < n-i-1; j++)
+                   if (arr[j]>arr[j + 1])
+                    {
+                        int temp = arr[j]; 
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+
+           
+       
+            foreach( var n2 in arr ) 
+                Console.WriteLine(n2);
+            
+            
+            Console.Read();
+
+        }
+    }
+}
